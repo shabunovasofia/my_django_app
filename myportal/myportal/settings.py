821231a3899d9f30cf49c5904056fdbf2e17669a
shabunovasofia <63457717+shabunovasofia@users.noreply.github.com>
2025-05-27 19:16:49 +0300

    Update settings.py

--- myportal/myportal/settings.py
+++ myportal/myportal/settings.py
@@ -147,7 +147,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis", 6379)],
+            "hosts": [(os.environ.get("REDIS_URL", "redis://redis:6379"))],
         },
     },
 }
